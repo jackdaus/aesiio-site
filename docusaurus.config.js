d25a522141815bdc1ca48898af72606403bdf446
Jack Daus <jackdaus@gmail.com>
2025-01-17 00:46:47 -0500

    Update socials

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,126 +1,130 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-import {themes} from 'prism-react-renderer';
+import { themes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: 'Aesiio',
-	tagline: 'A Blog Of Augmented Reality How-Tos For Beginners',
-	favicon: 'img/favicon.ico',
+  title: "Aesiio",
+  tagline: "A Blog Of Augmented Reality How-Tos For Beginners",
+  favicon: "img/favicon.ico",
 
-	// Set the production url of your site here
-	url: 'https://www.aesiio.com',
-	// Set the /<baseUrl>/ pathname under which your site is served
-	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: '/',
+  // Set the production url of your site here
+  url: "https://www.aesiio.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
 
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'aesiio', // Usually your GitHub org/user name.
-	projectName: 'aesiio-site', // Usually your repo name.
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "aesiio", // Usually your GitHub org/user name.
+  projectName: "aesiio-site", // Usually your repo name.
 
-	onBrokenLinks: 'throw',
-	onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
-	// Even if you don't use internalization, you can use this field to set useful
-	// metadata like html lang. For example, if your site is Chinese, you may want
-	// to replace "en" with "zh-Hans".
-	i18n: {
-		defaultLocale: 'en',
-		locales: ['en'],
-	},
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
 
-	presets: [
-		[
-			'classic',
-			/** @type {import('@docusaurus/preset-classic').Options} */
-			({
-				docs: {
-					sidebarPath: require.resolve('./sidebars.js'),
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					// editUrl:
-					// 	'https://github.com/jackdaus/aesiio-site/tree/main/docs',
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					// editUrl:
-					// 	'https://github.com/jackdaus/aesiio-site/tree/main',
-				},
-				theme: {
-					customCss: require.resolve('./src/css/custom.css'),
-				},
-			}),
-		],
-	],
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          // 	'https://github.com/jackdaus/aesiio-site/tree/main/docs',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          // 	'https://github.com/jackdaus/aesiio-site/tree/main',
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
 
-	themeConfig:
-		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-		({
-			image: 'img/aesiio.png',
-			navbar: {
-				title: 'Aesiio',
-				logo: {
-					alt: 'My Site Logo',
-					src: 'img/frogvr.png',
-				},
-				items: [
-					// {
-					// 	type: 'doc',
-					// 	docId: 'intro',
-					// 	position: 'left',
-					// 	label: 'Tutorial',
-					// },
-					{ to: '/blog', label: 'Blog', position: 'left' },
-					{ to: '/projects', label: 'Projects', position: 'left' },
-					{ to: '/about', label: 'About', position: 'right' },
-				],
-			},
-			footer: {
-				style: 'dark',
-				links: [
-					{
-						title: 'Social',
-						items: [
-							{
-								label: 'LinkedIn',
-								href: 'https://www.linkedin.com/in/jack-daus-25207583/',
-							},
-							{
-								label: 'Twitter',
-								href: 'https://twitter.com/aesiio',
-							},
-                            {
-                                label: 'YouTube',
-                                href: 'https://www.youtube.com/@jackdaus3124/featured'
-                            }
-						],
-					},
-					{
-						title: 'More',
-						items: [
-							{
-								label: 'About',
-								to: '/about',
-							},
-							{
-								label: 'GitHub',
-								href: 'https://github.com/jackdaus',
-							},
-						],
-					},
-				],
-				copyright: `© ${new Date().getFullYear()} Jack Daus`,
-			},
-			prism: {
-				theme: themes.github,
-				darkTheme: themes.dracula,
-                additionalLanguages: ['csharp','bash']
-			},
-		}),
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: "img/aesiio.png",
+      navbar: {
+        title: "Aesiio",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/frogvr.png",
+        },
+        items: [
+          // {
+          // 	type: 'doc',
+          // 	docId: 'intro',
+          // 	position: 'left',
+          // 	label: 'Tutorial',
+          // },
+          { to: "/blog", label: "Blog", position: "left" },
+          { to: "/projects", label: "Projects", position: "left" },
+          { to: "/about", label: "About", position: "right" },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Social",
+            items: [
+              {
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/in/jack-daus-25207583/",
+              },
+              {
+                label: "Bluesky",
+                href: "https://bsky.app/profile/aesiio.bsky.social",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/aesiio",
+              },
+              {
+                label: "YouTube",
+                href: "https://www.youtube.com/@jackdaus3124/featured",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "About",
+                to: "/about",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/jackdaus",
+              },
+            ],
+          },
+        ],
+        copyright: `© ${new Date().getFullYear()} Jack Daus`,
+      },
+      prism: {
+        theme: themes.github,
+        darkTheme: themes.dracula,
+        additionalLanguages: ["csharp", "bash"],
+      },
+    }),
 };
 
 export default config;
